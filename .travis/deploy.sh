5f79911cb55373216a9d173953b4aac5e4abc8d1
Brendan Koral <bkoral@dealerinspire.com>
2017-11-28 17:03:28 -0600

    Try the o flag

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,7 +9,7 @@ git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh -oStrictHostKeyChecking=no koralbuild@$IP -p $PORT <<EOF
+ssh -o StrictHostKeyChecking=no koralbuild@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
   npm install && pm2 restart bkoralio-server.js
 EOF
