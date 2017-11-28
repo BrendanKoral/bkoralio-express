783704f0025501861212c8c87a0d4f19e360f11d
Brendan Koral <bkoral@dealerinspire.com>
2017-11-28 17:23:28 -0600

    more testing

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Running deploy script"
+
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
