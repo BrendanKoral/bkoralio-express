6da9022aa55bd633b838ed23a1960833e6b5cec3
Brendan Koral <bkoral@dealerinspire.com>
2017-11-30 13:36:52 -0600

    Trying Josh setup

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,12 +6,21 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/deploy_key # Allow read access to the private key
 ssh-add .travis/deploy_key # Add the private key to SSH
 
-git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-git push deploy master
+
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -o StrictHostKeyChecking=no koralbuild@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
+  
+  if [ ! -d "personal-site" ]; then
+    # clone the repo on to the server
+    echo "Cloning repo from github"
+    git clone $REPO
+  else
+    # pull changes
+    echo "Pulling changes from github"
+    git pull
+  fi
+
   npm install && pm2 restart bkoralio-server.js
 EOF
