d2b4fb8f87de2dba1dfd6848dcbc9fe33097de38
Brendan Koral <bkoral@dealerinspire.com>
2017-11-30 21:37:23 -0600

    Adding color to server deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "Running deploy"
+CYAN='\033[0;36m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+echo -e "${CYAN}Running deploy ${NC} \n"
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/deploy_key # Allow read access to the private key
@@ -14,22 +18,24 @@ ssh -o StrictHostKeyChecking=no koralbuild@$IP -p $PORT <<EOF
   
   if [ ! -d $DEPLOY_DIR ]; then
     # clone the repo on to the server
-    echo "Cloning repo from github"
+    echo -e "${CYAN}Cloning repo from github ${NC} \n"
     git clone $REPO $DEPLOY_DIR
     cd $LIVE_DIR
   else
     # pull changes
-    echo "Pulling changes from github"
+    echo -e "${CYAN}Pulling changes from github ${NC}\n"
     cd $LIVE_DIR
     git pull
   fi
   
-  echo "INSTALLING NODE PACKAGES"
+  echo -e "${CYAN}INSTALLING NODE PACKAGES ${NC}\n"
   npm install
 
-  echo "UPDATING PACKAGES JUST TO BE SURE"
+  echo -e "${CYAN}UPDATING PACKAGES JUST TO BE SURE ${NC}\n"
   npm update
 
-  echo "RESTARTING BKORAL.IO SERVER"
+  echo -e "${CYAN}RESTARTING BKORAL.IO SERVER ${NC} \n"
   pm2 restart bkoralio-server.js
+
+  echo -e "${GREEN}Bkoral.io server has been rebuilt and restarted.${NC}"
 EOF
